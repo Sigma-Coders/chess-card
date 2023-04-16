afcb3a7d6eef4fbd3da41e99a3815d589e156092
Hayat Tamboli <hayat.tamboli@gmail.com>
2023-04-16 22:48:37 +0530

    added chess card logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
+import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Image
+      src={'/chess_cards_word_mark.png'}
+      alt='Chess-cards'
+      width={500}
+      height={100}
+      />
     </main>
   )
 }
